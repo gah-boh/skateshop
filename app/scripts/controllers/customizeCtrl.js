127df1d877d34513e4afa383864f8e6e74bc49e9
Gabriel Obregon <gabo.obregon@gmail.com>
2014-01-02 12:02:14 -0500

    Changed the boardSettings object from a local setting to a $scope object so it can be watched.

--- app/scripts/controllers/customizeCtrl.js
+++ app/scripts/controllers/customizeCtrl.js
@@ -1,4 +1,4 @@
-skateshop.controller('CustomizeCtrl', function(filterFilter, $rootScope, CustomizeSectionsFactory) {
+skateshop.controller('CustomizeCtrl', function($rootScope, $scope, filterFilter, CustomizeSectionsFactory) {
 	this.customizeSections = CustomizeSectionsFactory.sections;
 	this.boards = CustomizeSectionsFactory.board;
 	this.colors = CustomizeSectionsFactory.colors;
@@ -8,21 +8,27 @@ skateshop.controller('CustomizeCtrl', function(filterFilter, $rootScope, Customi
 		title: this.customizeSections[0]
 	};
 
-	this.boardSettings = {
+	$scope.boardSettings = {
 		boardLength: 0,
 		noseShape: 0,
 		tailShape: 0,
 		tape: 0
 	};
 
+	$scope.$watch('boardSettings.boardLength', function(newValue, oldValue, scope) {
+		if (newValue !== oldValue) {
+			console.log("testing");
+		};
+	});
+
 	this.selectBoardPreset = function(presetName) {
 		this.loadPreset(presetName);
-		$rootScope.$emit('boardPresetSelected', this.boardSettings);
+		$rootScope.$emit('boardPresetSelected', $scope.boardSettings);
 	};
 
 	this.loadPreset = function(presetName) {
 		var preset = filterFilter(this.boardPresets, {name: presetName})[0];
-		angular.extend(this.boardSettings, preset);
+		angular.extend($scope.boardSettings, preset);
 	};
 
 	this.loadPreset("street");
--- app/tests/customizeCtrlSpec.js
+++ app/tests/customizeCtrlSpec.js
@@ -13,37 +13,41 @@ describe("Customize Control Spec", function() {
 		$scope.$digest();
 	}));
 
-	// This test is to show an example of how to test and spy from a click
-	it("should call selectBoardPreset() on click", function() {
-		spyOn(element.scope().sut, 'selectBoardPreset').andCallThrough();
-		element.children().eq(0)[0].click();
-		expect(element.scope().sut.selectBoardPreset).toHaveBeenCalled();
-	});
+	describe("Presets", function() {
 
-	it("selectBoardPreset should call loadPreset", function() {
-		spyOn(sut, 'loadPreset');
-		sut.selectBoardPreset("street");
-		expect(sut.loadPreset).toHaveBeenCalled();
-	});
+		// This test is to show an example of how to test and spy from a click
+		it("should call selectBoardPreset() on click", function() {
+			spyOn(element.scope().sut, 'selectBoardPreset').andCallThrough();
+			element.children().eq(0)[0].click();
+			expect(element.scope().sut.selectBoardPreset).toHaveBeenCalled();
+		});
 
-	it("should have an image with the correct source", function() {
-		var image = element.children().eq(0).find('img');
-		expect(image.attr('src')).toBe('images/board_icon_street.png')
-	});
+		it("selectBoardPreset should call loadPreset", function() {
+			spyOn(sut, 'loadPreset');
+			sut.selectBoardPreset("street");
+			expect(sut.loadPreset).toHaveBeenCalled();
+		});
 
-	it("passing a preset by name should get the correct boardLength from the factory", function() {
-		sut.loadPreset("street");
-		expect(sut.boardSettings.boardLength).toBe(30);
-	});
-	
-	it("passing a preset by name should get the correct noseShape from the factory", function() {
-		sut.loadPreset("street");
-		expect(sut.boardSettings.noseShape).toBe(50);
-	});
+		it("should have an image with the correct source", function() {
+			var image = element.children().eq(0).find('img');
+			expect(image.attr('src')).toBe('images/board_icon_street.png')
+		});
 
-	it("passing a preset by name should get the correct tailShape from the factory", function() {
-		sut.loadPreset("street");
-		expect(sut.boardSettings.tailShape).toBe(50);
+		it("passing a preset by name should get the correct boardLength from the factory", function() {
+			sut.loadPreset("street");
+			expect($scope.boardSettings.boardLength).toBe(30);
+		});
+		
+		it("passing a preset by name should get the correct noseShape from the factory", function() {
+			sut.loadPreset("street");
+			expect($scope.boardSettings.noseShape).toBe(50);
+		});
+
+		it("passing a preset by name should get the correct tailShape from the factory", function() {
+			sut.loadPreset("street");
+			expect($scope.boardSettings.tailShape).toBe(50);
+		});
+
 	});
 
 });
