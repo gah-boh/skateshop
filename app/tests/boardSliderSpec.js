04a877c9b9983b1954d708ce6d81bb81e1456fb1
Gabriel Obregon <gabo.obregon@gmail.com>
2014-01-10 14:08:09 -0500

    Changed board slider tests to not use a mock controller.

--- app/tests/boardSliderSpec.js
+++ app/tests/boardSliderSpec.js
@@ -3,24 +3,16 @@ describe("Board Slider Specs", function() {
 	var $rootScope,
 		$scope,
 		element,
-		elementIsolatedScope,
-		mockCtrl;
+		elementIsolatedScope;
 
-	beforeEach(module(function($provide) {
-		var mockController = function() {
-			this.currentValue = 3;
-		};
-		angular.module("Skateshop").controller('MockCtrl', mockController);
-		$provide.value('MockCtrl',  mockController);
-	}));
 	beforeEach(module("Skateshop"));
-	beforeEach(inject(function(_$rootScope_, $compile, $controller) {
+	beforeEach(inject(function(_$rootScope_, $compile) {
 		$rootScope = _$rootScope_;
 		$scope = _$rootScope_.$new();
-		element = angular.element('<div ng-controller="MockCtrl as mockCtrl"><div board-slider="mockCtrl.currentValue" min="45" max="90"></div></div>');
+		$scope.currentValue = 3;
+		element = angular.element('<div board-slider="currentValue" min="45" max="90"></div>');
 		$compile(element)($scope);
-		mockCtrl = element.scope().mockCtrl;
-		elementIsolatedScope = element.children().eq(0).isolateScope();
+		elementIsolatedScope = element.eq(0).isolateScope();
 	}));
 
 	afterEach(function() {
@@ -29,22 +21,22 @@ describe("Board Slider Specs", function() {
 
 	it("should change the controller's value when value the update function is called", function() {
 		elementIsolatedScope.update(4);
-		expect(mockCtrl.currentValue).toBe(4);
+		expect($scope.currentValue).toBe(4);
 	});
 
 	it("should update the slider value on a model change", function() {
 		spyOn(elementIsolatedScope, 'updateSlider');
-		mockCtrl.currentValue = 10;
+		$scope.currentValue = 10;
 		$scope.$digest();
 		expect(elementIsolatedScope.updateSlider).toHaveBeenCalled();
 	});
 
 	it("should have a minimum of 45 as supplied by the element", function() {
-		expect(element.children(0).eq(0).slider("option", "min")).toBe(45);
+		expect(element.eq(0).slider("option", "min")).toBe(45);
 	});
 
 	it("should have a maximum of 90 as supplied by the element", function() {
-		expect(element.children(0).eq(0).slider("option", "max")).toBe(90);
+		expect(element.eq(0).slider("option", "max")).toBe(90);
 	});
 
 });
